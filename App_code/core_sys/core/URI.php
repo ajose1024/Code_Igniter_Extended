d47c8374cc36c1ff4f0b1ba99341c34d237bc9bb
António José Gomes <ajose.gomes@ajgomes.datanet-pt.net>
2017-02-05 01:47:05 +0000

    20170205.0145 -- Code refactoring and editing

--- App_code/core_sys/core/URI.php
+++ App_code/core_sys/core/URI.php
@@ -187,64 +187,64 @@ class CI_URI
 
     // --------------------------------------------------------------------
 
-	/**
-	 * Parse REQUEST_URI
-	 *
-	 * Will parse REQUEST_URI and automatically detect the URI from it,
-	 * while fixing the query string if necessary.
-	 *
-	 * @return	string
-	 */
-	protected function _parse_request_uri()
-	{
-		if(  ! isset($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']))
-		{
-			return '';
-		}
+    /**
+     * Parse REQUEST_URI
+     *
+     * Will parse REQUEST_URI and automatically detect the URI from it,
+     * while fixing the query string if necessary.
+     *
+     * @return	string
+     */
+    protected function _parse_request_uri()
+    {
+        if(  ! isset($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']))
+        {
+            return '';
+        }
 
-		// parse_url() returns false if no host is present, but the path or query string
-		// contains a colon followed by a number
-		$uri = parse_url('http://dummy'.$_SERVER['REQUEST_URI']);
-		$query = isset($uri['query']) ? $uri['query'] : '';
-		$uri = isset($uri['path']) ? $uri['path'] : '';
+        // parse_url() returns false if no host is present, but the path or query string
+        // contains a colon followed by a number
+        $uri = parse_url('http://dummy'.$_SERVER['REQUEST_URI']);
+        $query = isset($uri['query']) ? $uri['query'] : '';
+        $uri = isset($uri['path']) ? $uri['path'] : '';
 
-		if( isset($_SERVER['SCRIPT_NAME'][0]))
-		{
-			if( strpos($uri, $_SERVER['SCRIPT_NAME']) === 0)
-			{
-				$uri = (string) substr($uri, strlen($_SERVER['SCRIPT_NAME']));
-			}
-			elseif( strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0)
-			{
-				$uri = (string) substr($uri, strlen(dirname($_SERVER['SCRIPT_NAME'])));
-			}
-		}
+        if( isset($_SERVER['SCRIPT_NAME'][0]))
+        {
+            if( strpos($uri, $_SERVER['SCRIPT_NAME']) === 0)
+            {
+                $uri = (string) substr($uri, strlen($_SERVER['SCRIPT_NAME']));
+            }
+            elseif( strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0)
+            {
+                $uri = (string) substr($uri, strlen(dirname($_SERVER['SCRIPT_NAME'])));
+            }
+        }
 
-		// This section ensures that even on servers that require the URI to be in the query string (Nginx) a correct
-		// URI is found, and also fixes the QUERY_STRING server var and $_GET array.
-		if( trim($uri, '/') === '' && strncmp($query, '/', 1) === 0)
-		{
-			$query = explode('?', $query, 2);
-			$uri = $query[0];
-			$_SERVER['QUERY_STRING'] = isset($query[1]) ? $query[1] : '';
-		}
-		else
-		{
-			$_SERVER['QUERY_STRING'] = $query;
-		}
+        // This section ensures that even on servers that require the URI to be in the query string (Nginx) a correct
+        // URI is found, and also fixes the QUERY_STRING server var and $_GET array.
+        if( trim($uri, '/') === '' && strncmp($query, '/', 1) === 0)
+        {
+            $query = explode('?', $query, 2);
+            $uri = $query[0];
+            $_SERVER['QUERY_STRING'] = isset($query[1]) ? $query[1] : '';
+        }
+        else
+        {
+            $_SERVER['QUERY_STRING'] = $query;
+        }
 
-		parse_str($_SERVER['QUERY_STRING'], $_GET);
+        parse_str($_SERVER['QUERY_STRING'], $_GET);
 
-		if( $uri === '/' OR $uri === '')
-		{
-			return '/';
-		}
+        if( $uri === '/' OR $uri === '')
+        {
+            return '/';
+        }
 
-		// Do some final cleaning of the URI and return it
-		return $this->_remove_relative_directory($uri);
-	}
+        // Do some final cleaning of the URI and return it
+        return $this->_remove_relative_directory($uri);
+    }
 
-	// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
 	/**
 	 * Parse QUERY_STRING
